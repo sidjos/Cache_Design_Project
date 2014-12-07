a11d6f8c774ddd21d6ff92e2296835dadde194c8
S-H.C <Shao-Han.Chen@u.northwestern.edu>
2014-12-06 21:01:18 -0600

    Update 6_b_main_memory.vhd

--- 6_b_main_memory.vhd
+++ 6_b_main_memory.vhd
@@ -23,20 +23,25 @@ architecture structural of main_memory is
 signal mux0,mux1,mux2,mux3,mux4,shifter,fulladder1: std_logic_vector(2047 downto 0);
 signal mux6: std_logic_vector(9 downto 0);
 signal syncram0,counter,pc0: std_logic_vector(31 downto 0);
-signal not0: std_logic;
+signal not0,clk_new1,clk_new2,not_clk_new2,not_L1_Miss: std_logic;
 
 
 begin
     
-  --main memory 
+   --reset
+   not1_map: not_gate port map (x=>L1_Miss, z=>not_L1_Miss);
+   or1_map:  or_gate port map (x=>reset, y=>not_L1_Miss, z=>clk_new1);
+    
+    
+   --main memory 
    mux6_map: 	mux_n generic map (n=>10) port map (sel=>write, src0=>B"0000000000", src1=>addr(9 downto 0), z=>mux6);
    syncram_map:	syncram (mem_file => memfile_s)
-   		port map (clk=>, cs=>'1', oe=>'1', we=>write, addr(31 downto 10)=>addr(31 downto 10), addr(9 downto 0)=>mux6, din=>data_in, dout=>syncram0);
+   		port map (clk=>clk_new2, cs=>'1', oe=>'1', we=>write, addr(31 downto 10)=>addr(31 downto 10), addr(9 downto 0)=>mux6, din=>data_in, dout=>syncram0);
   
    --32 bits counter (positive edge)
    fulladder0_map:  fulladder_32 port map (cin=>'0', x=>pc0, y=>B"00000000000000000000000000000001", z=>counter);
    generate_memory0: for i in 0 to 31 generate
-   map_memory_reg0: dffr_a port map (clk=>clk, arst=>reset, aload=>'0', adata=>'0', d=>fulladder0(i), enable=>'1',q=>pc0(i));
+   map_memory_reg0: dffr_a port map (clk=>clk_new2, arst=>reset, aload=>'0', adata=>'0', d=>fulladder0(i), enable=>'1',q=>pc0(i));
    end generate_memory0;
    
    --shifter
@@ -56,18 +61,17 @@ begin
    --temporary 256 bytes Registers (negative edge)
    
    fulladder1_map:  fulladder_n generic map (n=>2048) port map (cin=>'0', x=>data_out, y=>shifter, z=>fulladder1);
-   not0_map:	not_gate port map (x=>clk,z=>not_clk);
+   not0_map:	not_gate port map (x=>clk_new2,z=>not_clk_new2);
    generate_memory1: for i in 0 to 2047 generate
    map_memory_reg1: dffr_a port map (clk=>not_clk, arst=>rst,aload=>'0', adata=>'0', d=>fulladder1(i), enable=>'1',q=>data_out(i));
    end generate_memory1;
    
    --valid 
-   and0_map: and_gate port map (x=>counter(0), y=>counter(1), z=>and0);
-   and1_map: and_gate port map (x=>and0, y=>counter(2), z=>and1);
-   and2_map: and_gate port map (x=>and1, y=>counter(3), z=>and2);
-   and3_map: and_gate port map (x=>and2, y=>counter(4), z=>and3);
-   and4_map: and_gate port map (x=>and3, y=>counter(5), z=>and4);
+   and0_map: and_gate port map (x=>counter(6), y=>counter(0), z=>data_valid);
+   or0_map:  or_gate port map (x=>clk, y=>data_valid, z=>clk_new1);
+   and1_map:  or_gate port map (x=>clk_new1, y=>L2_Miss, z=>clk_new2);
    
+  
    
 end architecture structural;
 
