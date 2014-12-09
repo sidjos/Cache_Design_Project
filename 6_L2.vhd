a3dd1084a344da6ca02e0696cf90e198923679c5
Siddhartha Joshi <sidjos@gmail.com>
2014-12-09 03:51:20 -0600

    le LRU

--- 6_L2.vhd
+++ 6_L2.vhd
@@ -42,7 +42,8 @@ component tag_L2_compare is
 		input_3:	in std_logic_vector(2069 downto 0);
 		tag:		in std_logic_vector(21 downto 0);
 		output:		out std_logic_vector(2069 downto 0);
-		hit:		out std_logic
+		hit:		out std_logic;
+		comp:	out std_logic_vector(3 downto 0)
 	);
 end component;
 
@@ -64,12 +65,24 @@ component lru_counter_to_offset_s is
     Wr_o   : out std_logic_vector(3 downto 0)
     );
   end component;
+  
+component lru_counter_to_offset_s is
+  port ( 
+    Update : in std_logic;
+    Rd     : in std_logic_vector(3 downto 0);
+    Wr     : in std_logic_vector(3 downto 0);
+    Reset  : in std_logic;
+    Wr_o   : out std_logic_vector(3 downto 0)
+    );
+  end component;
 
     signal tag_L2 : std_logic_vector (21 downto 0);
     signal index_L2: std_logic_vector ( 1 downto 0);
     signal offset_L2, offset_inv: std_logic_vector ( 7 downto 0);
     
-    signal WrEn_L2, WrEn_L2_s0_pc, WrEn_L2_s1_pc, WrEn_L2_s2_pc, WrEn_L2_s3_pc, WrEn_L2_s0, WrEn_L2_s1, WrEn_L2_s2, WrEn_L2_s3, L2_tag_match,L2_tag_miss, h0, h1 : std_logic;
+    signal set_read, set_written, set_to_be_written: std_logic_vector(3 downto 0);
+    
+    signal update, WrEn_L2, WrEn_L2_s0_pc, WrEn_L2_s1_pc, WrEn_L2_s2_pc, WrEn_L2_s3_pc, WrEn_L2_s0, WrEn_L2_s1, WrEn_L2_s2, WrEn_L2_s3, L2_tag_match,L2_tag_miss, h0, h1 : std_logic;
     signal L2_Block_Out_s0, L2_Block_Out_s1, L2_Block_Out_s2, L2_Block_Out_s3, tag, L2_block_Out, L2_Block_In  : std_logic_vector ( 2069 downto 0);
     signal b0, b1, b2, b3, m0, m1 : std_logic_vector (511 downto 0);
  --   signal m0, m1, m2, m3, s1, s0, L1_hit_block_In_wdt, L1_Block_In_wdt, L1_Block_shifted : std_logic_vector ( 511 downto 0);
@@ -96,7 +109,7 @@ L2_csram_s3: csram generic map ( INDEX_WIDTH => 2, BIT_WIDTH => 2070 )
 
 --Comparators
 
-L2_output_current: tag_L2_compare port map ( L2_Block_Out_s0, L2_Block_Out_s1, L2_Block_Out_s2, L2_Block_Out_s3, tag_L2, L2_block_Out, L2_tag_match);
+L2_output_current: tag_L2_compare port map ( L2_Block_Out_s0, L2_Block_Out_s1, L2_Block_Out_s2, L2_Block_Out_s3, tag_L2, L2_block_Out, L2_tag_match, set_read);
 miss_sig_map_L2: not_gate port map (L2_tag_match, L2_tag_miss);
 
 --output
@@ -126,7 +139,18 @@ hitmap2: or_gate port map ( h0, h1, WrEn_L2);
 
 --------------------------------
 --Where to write/ LRU Implementation
-WrEn_L2_s0_pc <= WrEn_L2;
+--WrEn_L2_s0_pc <= WrEn_L2;
+
+--set_written <= "0000";
+
+WrEn_L2_s0_pc <= set_to_be_written(0);
+WrEn_L2_s1_pc <= set_to_be_written(1);
+WrEn_L2_s2_pc <= set_to_be_written(2);
+WrEn_L2_s3_pc <= set_to_be_written(3);
+
+update_LRU_map: or_gate port map (L2_tag_match, WrEn_L2, update);
+LRU_Map: lru_counter_to_offset_s port map ( update, set_read, set_to_be_written, Enable, set_to_be_written);
+
 --Initialize LRU here and comment the above line. Whatever output the LRU is giving, AND it with
 --WrEn_L2 and we will have the write signals for the four sets. 
 --------------------------------
