472013c3c8adc16b2a71f1924bf95c01197ba022
Siddhartha Joshi <sidjos@gmail.com>
2014-12-11 13:51:13 +0530

    Counters work !!!!
    MUmbai Commit

--- 6_Memory_Heirarchy.vhd
+++ 6_Memory_Heirarchy.vhd
@@ -104,7 +104,7 @@ port(
 end component;
 
 signal L2_Block_Out, Data_L1: std_logic_vector ( 511 downto 0);
-signal Write_Main_Memory, Memory_Write_Complete, Dirty_Bit_Evict,EN_C, Reset_for_Main_Memory, Ready_Sig, L2_Data_Valid, memory_data_valid, L2_Hit, L1_Hit, L1_Miss, L2_Miss, L1_Hit_sync, L1_Miss_sync, L2_Miss_sync, L2_Hit_sync: std_logic; 
+signal Write_Main_Memory, Ready_Sig_Comp, Memory_Write_Complete, Dirty_Bit_Evict,EN_C, Reset_for_Main_Memory, Ready_Sig, L2_Data_Valid, memory_data_valid, L2_Hit, L1_Hit, L1_Miss, L2_Miss, L1_Hit_sync, L1_Miss_sync, L2_Miss_sync, L2_Hit_sync: std_logic; 
 signal Memory_Block_In: std_logic_vector (2069 downto 0);
 
 
@@ -113,18 +113,22 @@ begin
 
 ready_signal_map: syncboss port map (clk, L1_Hit, EN_C, Ready_Sig);
 Ready <= Ready_Sig; 
-L1_Hit_Count_s: syncboss port map (clk, L1_Hit, L1_Hit_sync);
-L1_Miss_Count_s: syncboss port map (clk, L1_Miss, L1_Miss_sync);
-L2_Hit_Count_s: syncboss port map (clk, L2_Hit, L2_Hit_sync);
-L2_Miss_Count_s: syncboss port map (clk, L2_Miss, L2_Miss_sync);
+ready_comp_map: not_gate port map (Reset_for_Main_Memory, Ready_Sig_Comp);
+
+L1_Hit_Count_s: syncboss port map (Ready_Sig_Comp, L1_Hit, EN_C, L1_Hit_sync);
+L1_Miss_Count_s: syncboss port map (Ready_Sig_Comp, L1_Miss, EN_C, L1_Miss_sync);
+L2_Hit_Count_s: syncboss port map (Ready_Sig_Comp, L2_Hit, EN_C, L2_Hit_sync);
+L2_Miss_Count_s: syncboss port map (Ready_Sig_Comp, L2_Miss, EN_C, L2_Miss_sync);
 
 enable_comp: not_gate port map (EN, EN_C);
 Reset_Main_MeM_MAP: or_gate port map (rst, Ready_Sig, Reset_for_Main_Memory);
 
-L1_Hit_Counter: Counter_S port map ('1', L1_Hit_sync, '0', l1_hit_cnt);
-L1_Miss_Counter: Counter_S port map ('1', L1_Miss_sync, '0', l1_miss_cnt);
-L1_Evict_Counter: Counter_S port map ('1', Dirty_Bit_Evict, '0', l1_evict_cnt);
-
+L1_Hit_Counter: Counter_S port map (L1_Hit, Ready_Sig_Comp, rst, l1_hit_cnt);
+L1_Miss_Counter: Counter_S port map (L1_Miss, Ready_Sig_Comp, rst, l1_miss_cnt);
+L1_Evict_Counter: Counter_S port map (Dirty_Bit_Evict, Ready_Sig_Comp, rst, l1_evict_cnt);
+L2_Hit_Counter: Counter_S port map (L2_Hit, Ready_Sig_Comp, rst, l2_hit_cnt);
+L2_Miss_Counter: Counter_S port map (L2_Miss, Ready_Sig_Comp, rst, l2_miss_cnt);
+L2_Evict_Counter: Counter_S port map (memory_write_complete, Ready_Sig_Comp, rst, l2_evict_cnt);
 
 
 L1_map: L1 port map
